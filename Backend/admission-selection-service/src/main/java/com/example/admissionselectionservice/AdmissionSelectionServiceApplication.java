1601f99a46379e45f76f226e07435447bfcccd38
Divya <divya@Divyas-MacBook-Air.local>
2021-11-25 12:25:03 +0530

    discovery client set up

--- Backend/admission-selection-service/src/main/java/com/example/admissionselectionservice/AdmissionSelectionServiceApplication.java
+++ Backend/admission-selection-service/src/main/java/com/example/admissionselectionservice/AdmissionSelectionServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AdmissionSelectionServiceApplication {
 
 	public static void main(String[] args) {
